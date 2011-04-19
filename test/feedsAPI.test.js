45302316786f14cfcb3b983741647ee42948bc1d
Emmanuel Prochasson <eprochasson@gmail.com>
2011-04-19 18:11:19 +0800

    add tests

--- test/feedsAPI.test.js
+++ test/feedsAPI.test.js
@@ -24,7 +24,7 @@ describe("FeedsAPI",function(){
                    expect(req.get).toHaveBeenCalledWith(
                    { consumerKey:"Key", consumerSecret: "Secret", authHandler : 'oauth', apiHost:"api.echoenabled.com"},
                    '/v1/feeds/list',
-                   {},
+                   "",
                    callback)
                 })
             });
