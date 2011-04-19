1466e04871c6625ae3bf69b9288233e4ad8189a1
Emmanuel Prochasson <eprochasson@gmail.com>
2011-04-19 19:23:35 +0800

    bug spotted !

---
+++ test/bug.test.js
@@ -0,0 +1,16 @@
+var Bugs = require("Buggy.js");
+var BuggyCall = require("bug.js").MyBuggyCall;
+
+describe("bug.js",function(){
+    beforeEach(function(){
+        spyOn(Bugs.SuperBuggy,"spawn").andCallThrough();
+        buggycall = new (BuggyCall);
+        buggycall.bug()
+    });
+    it("should have called spawn",function(){
+        expect(Bugs.SuperBuggy.spawn).toHaveBeenCalled()
+    });
+    it("should have called spawn with the expected parameters",function(){
+        expect(Bugs.SuperBuggy.spawn).toHaveBeenCalledWith({property:"a regular property"});
+    })
+});
