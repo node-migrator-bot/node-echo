228f7da075a050649ab762b5a26ce4752323460b
Ed Bosher <edbosher@gmail.com>
2011-04-20 12:41:10 +0800

    removed duplicate describe

--- test/bug.test.js
+++ test/bug.test.js
@@ -4,27 +4,22 @@ var BuggyCall = require("bug.js").MyBuggyCall;
 describe("SuperBuggy",function(){
     beforeEach(function(){
         spyOn(Bugs.SuperBuggy,"spawn").andCallThrough();
-        buggycall = new (BuggyCall);
-        buggycall.bug()
-    });
-    it("should have called spawn",function(){
-        expect(Bugs.SuperBuggy.spawn).toHaveBeenCalled()
-    });
-    it("should have called spawn with the expected parameters",function(){
-        expect(Bugs.SuperBuggy.spawn).toHaveBeenCalledWith({property:{content:"a regular property"}});
-    })
-});
-
-describe("Buggy",function(){
-    beforeEach(function(){
         spyOn(Bugs.Buggy,"spawn").andCallThrough();
         buggycall = new (BuggyCall);
         buggycall.bug()
     });
+
     it("should have called spawn",function(){
         expect(Bugs.Buggy.spawn).toHaveBeenCalled()
     });
     it("should have called spawn with the expected parameters",function(){
         expect(Bugs.Buggy.spawn).toHaveBeenCalledWith({property:{content:"a regular property"}});
+    });
+
+    it("should have called spawn",function(){
+        expect(Bugs.SuperBuggy.spawn).toHaveBeenCalled()
+    });
+    it("should have called spawn with the expected parameters",function(){
+        expect(Bugs.SuperBuggy.spawn).toHaveBeenCalledWith({property:{content:"a regular property"}});
     })
 });
